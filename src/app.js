3ca94dcb9c418e29d6c310772c8f733e779d4ab3
JMP160880 <javiermoranpando@gmail.com>
2023-01-04 05:53:27 +0000

    4-1-23

--- src/app.js
+++ src/app.js
@@ -10,13 +10,13 @@ window.onload = () => {
 
 let generateExcuse = () => {
   let who = ["the dog", "my granma", "his turtle", "my bird"];
-  let what = ["eat", "pissed", "crushed", "broked"];
+  let what = [" eat", " pissed", " crushed", " broked"];
   let when = [
-    "before the class",
-    "right in time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
+    " before the class",
+    " right in time",
+    " when I finished",
+    " during my lunch",
+    " while I was praying"
   ];
   let whoIndex = Math.floor(Math.random() * who.length);
   let whatIndex = Math.floor(Math.random() * what.length);
